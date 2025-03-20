0bd5d8bff8efda619a9f2ed85deebade3b391ea6
Kacper Talaga <k.talaga3@gmail.com>
2025-03-20 13:55:39 +0100

    Vercel integration

--- management_system/management_system/settings.py
+++ management_system/management_system/settings.py
@@ -27,9 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-PORT = int(os.getenv("PORT", 5000))
-ALLOWED_HOSTS = ["management-system.onrender.com", "127.0.0.1", "*", "0.0.0.0"]
+DEBUG = True
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -129,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
